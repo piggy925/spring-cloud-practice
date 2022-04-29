7d10f1fa1702c28448df2c7d5a657b4b41003ffe
MuMu <piggy925@163.com>
2022-04-29 20:10:43 +0800

    feat：整合 Nacos Config 配置中心

--- order/src/main/java/com/mumu/order/OrderApplication.java
+++ order/src/main/java/com/mumu/order/OrderApplication.java
@@ -3,6 +3,9 @@ package com.mumu.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author mumu
@@ -14,7 +17,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class OrderApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderApplication.class, args);
+        while (true) {
+            String name = applicationContext.getEnvironment().getProperty("user.name");
+            String age = applicationContext.getEnvironment().getProperty("user.age");
+            String sex = applicationContext.getEnvironment().getProperty("config.sex");
+            System.out.println("name: " + name);
+            System.out.println("age: " + age);
+            TimeUnit.SECONDS.sleep(1);
+            
+        }
     }
 }
